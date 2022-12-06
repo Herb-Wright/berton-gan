f1b9360bab4206863f50fd027cad7f3d57dee4dc
Aaron Schindler <legolover1000@gmail.com>
2022-12-06 16:57:25 -0700

    Added face encoder for mnist

--- src/networks.py
+++ src/networks.py
@@ -48,4 +48,38 @@ class BertonGan():
 		'''
 		pass
 
+	def face_encoder():
+		'''
+		This is a face encoder network using nn.sequential
+
+		The layers are as follows:
+		1. Convolutional layer with 32 filters, 5x5 size, stride 1
+		2. Leaky ReLU
+		3. 2x2 Max pooling layer, stride 2
+		4. Convolutional layer with 64 filters, 5x5 size, stride 1
+		5. Leaky ReLu
+		6. 2x2 Max pooling layer, stride 2
+		7. Flattening the matrix before the fully connected layer
+		8. Fully connected layer with output size 4x4x64
+		9. Leaky ReLU
+		10. Fully connected layer with output size of 2
+		'''
+		return nn.Sequential(
+    		nn.Conv2d(1,32,5,1),
+    		nn.LeakyReLU(0.01),
+    		nn.MaxPool2d(2,2),
+    		nn.Conv2d(32,64,5,1),
+    		nn.LeakyReLU(0.01),
+    		nn.MaxPool2d(2,2),
+    		Flatten(),
+    		nn.Linear(1024,4*4*64),
+    		nn.LeakyReLU(0.01),
+    		nn.Linear(4*4*64,2)
+  		)
+	
+class Flatten(nn.Module):
+	def forward(self, x):
+		N, C, H, W = x.size()
+		return x.view(N, -1)
+
 
